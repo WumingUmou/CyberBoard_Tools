7475e5d92d97148e25f9b759b8081b796d78c892
WumingUmou <73727350+WumingUmou@users.noreply.github.com>
2022-08-16 10:49:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("Frame Inverter")
 st.write("Upload your json file here")
 read_file = st.file_uploader("upload your json", "json", False, )
 if read_file is not None:
-    file = json.load(file)
+    file = json.load(read_file)
     st.write("file read success")
