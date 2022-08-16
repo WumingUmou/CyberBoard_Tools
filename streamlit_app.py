933268bb73f76b0ca12444f75a3732cbf036d08b
WumingUmou <73727350+WumingUmou@users.noreply.github.com>
2022-08-16 10:53:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ read_file = st.file_uploader("upload your json", "json", False, )
 if read_file is not None:
     file = json.load(read_file)
     st.write("file read success")
+    targets = st.multiselect("Which page you want to invert?", ["自定义界面1/custom page1", "自定义界面2/custom page2", "自定义界面3/custom page3"],None)
+    
+    targets = [x.split("/")[0] for x in targets]
+    st.write(targets)
+    
